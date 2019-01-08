755b894fdf26acf77ff05b0506ef6019fe6a33ca
Alasdair Smith <alasdairsmith0491@gmail.com>
2019-01-08 12:25:37 +0000

    Refactor: replaced individual tests with parameterized test.

--- src/test/java/ConvertsNumeralsToDecimalTest.java
+++ src/test/java/ConvertsNumeralsToDecimalTest.java
@@ -1,25 +1,21 @@
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
 import static org.junit.Assert.assertEquals;
 
+@RunWith(JUnitParamsRunner.class)
 public class ConvertsNumeralsToDecimalTest {
-    @Test
-    public void converts_numeral_I_to_decimal_1() {
-        assertEquals(1, NumeralConverter.convert("I"));
-    }
 
     @Test
-    public void converts_numerals_II_to_decimal_2() {
-        assertEquals(2, NumeralConverter.convert("II"));
-    }
-
-    @Test
-    public void converts_numerals_III_to_decimal_3() {
-        assertEquals(3, NumeralConverter.convert("III"));
-    }
-
-    @Test
-    public void converts_numeral_V_to_decimal_5() {
-        assertEquals(5, NumeralConverter.convert("V"));
+    @Parameters({
+            "I, 1",
+            "II, 2",
+            "III, 3",
+            "V, 5",
+    })
+    public void converts_numerals_to_decimal(String numeral, int decimal) {
+        assertEquals(decimal, NumeralConverter.convert(numeral));
     }
 }
