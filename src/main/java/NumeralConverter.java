21501b9aaa8ffcf6990782800f08785bc36c02a0
Alasdair Smith <alasdairsmith0491@gmail.com>
2019-01-08 12:52:02 +0000

    Refactor: replaced string 'I' with constants.

--- src/main/java/NumeralConverter.java
+++ src/main/java/NumeralConverter.java
@@ -1,15 +1,16 @@
 public class NumeralConverter {
 
     public static final String NUMERAL_V = "V";
+    public static final String NUMERAL_I = "I";
 
     public static int convert(String numerals) {
-        if(numerals.equals("VIII"))
+        if(numerals.equals("V" + NUMERAL_I + "II"))
             return 8;
-        if(numerals.equals("VII"))
+        if(numerals.equals("V" + NUMERAL_I + "I"))
             return 7;
-        if(numerals.equals("VI"))
+        if(numerals.equals("V" + NUMERAL_I))
             return 6;
-        if(numerals.equals("IV"))
+        if(numerals.equals(NUMERAL_I + "V"))
             return 4;
         if(numerals.equals(NUMERAL_V))
             return 5;
