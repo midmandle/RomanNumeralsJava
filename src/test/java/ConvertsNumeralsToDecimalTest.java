97f9baab19232d13db88de576748f4e497a54d6c
Alasdair Smith <alasdairsmith0491@gmail.com>
2019-01-08 16:09:09 +0000

    Feature: can convert XLV to 45

--- src/test/java/ConvertsNumeralsToDecimalTest.java
+++ src/test/java/ConvertsNumeralsToDecimalTest.java
@@ -26,6 +26,7 @@ public class ConvertsNumeralsToDecimalTest {
             "XIV, 14",
             "L, 50",
             "XL, 40",
+            "XLV, 45",
     })
     public void converts_numerals_to_decimal(String numeral, int decimal) {
         assertEquals(decimal, NumeralConverter.convert(numeral));
